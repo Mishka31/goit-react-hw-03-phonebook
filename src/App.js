1259e760b7e4df9e3813f0a4470669d09771dff2
Misha Krasnonos <muzishutka7@gmail.com>
2021-09-04 00:52:27 +0300

    add hw 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ContactForm from "./ContactForm/ContactForm.jsx";
+import ContactList from "./ContactList/ContactList.jsx";
+import Filter from "./Filter/Filter.jsx";
+import s from "./App.module.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { v4 as uuidv4 } from "uuid";
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "c2cc433e-97d2-4fa7-b9eb-12a98511c9ab", name: "Misha Krasnonos", number: "000-00-11-11" },
+      { id: "f98d00d6-01b7-481a-8b52-c607c5127348", name: "Sasha Krasnonos", number: "111-11-00-00" },
+      { id: "f98d00d6-01b7-481a-8b52-c607c7887 348", name: "Lena Krasnonos", number: "111-11-22-22" },
+    ],
+    filter: "",
+  };
+
+  formSubmitHandler = (data) => {
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, { id: uuidv4(), ...data }],
+    }));
+  };
+
+  onDelete = (id) => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter((contact) => contact.id !== id.target.id),
+    }));
+  };
+
+  onFind = (e) => {
+    this.setState({
+      filter: e.target.value,
+    });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const lowerFilter = this.state.filter.toLowerCase();
+    const filteredContacts = this.state.contacts.filter((contact) => contact.name.toLowerCase().includes(lowerFilter));
+
+    return (
+      <div>
+        <h1 className={s.titleH1}>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} listArrey={contacts} />
+        <h2 className={s.titleH2}>Contacts</h2>
+        <Filter value={filter} onSearch={this.onFind} />
+        <ContactList contacts={filteredContacts} onDelete={this.onDelete} />
+      </div>
+    );
+  }
 }
 
 export default App;
